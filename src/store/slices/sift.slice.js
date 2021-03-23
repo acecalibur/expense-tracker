cdf9483c1d4b98428319b6615a4ba262fb624150
tdubsxc <traviedubs@gmail.com>
2021-03-22 22:22:17 -0400

    Wired up the sift actions and reducer.

--- src/store/slices/sift.slice.js
+++ src/store/slices/sift.slice.js
@@ -1,6 +1,6 @@
 import produce from 'immer';
 
-// Types
+// Action Types
 const SET_SEARCH = 'sift/setSearch';
 const SORT_BY_DATE = 'sift/sortByDate';
 const SORT_BY_AMOUNT = 'sift/sortByAmount';
@@ -9,23 +9,61 @@ const SET_START_DATE = 'sift/setStartDate';
 const SET_END_DATE = 'sift/setEndDate';
 
 // Action Creators
+export const siftActions = {
+  setSearch: (text) => ({
+    type: SET_SEARCH,
+    payload: text,
+  }),
+  sortByDate: (value) => ({
+    type: SORT_BY_DATE,
+    payload: value,
+  }),
+  sortByAmount: (value) => ({
+    type: SORT_BY_AMOUNT,
+    payload: value,
+  }),
+  setCategory: (category) => ({
+    type: SET_CATEGORY,
+    payload: category,
+  }),
+  setStartDate: (startDate) => ({
+    type: SET_START_DATE,
+    payload: startDate,
+  }),
+  setEndDate: (endDate) => ({
+    type: SET_END_DATE,
+    payload: endDate,
+  }),
+};
 
 // Reducers
 const siftReducer = produce(
   (draft, { type, payload }) => {
     switch (type) {
-      case SET_SEARCH:
+      case SET_SEARCH: {
+        draft.search = payload;
         break;
-      case SORT_BY_DATE:
+      }
+      case SORT_BY_DATE: {
+        draft.sortBy = payload;
         break;
-      case SORT_BY_AMOUNT:
+      }
+      case SORT_BY_AMOUNT: {
+        draft.sortBy = payload;
         break;
-      case SET_CATEGORY:
+      }
+      case SET_CATEGORY: {
+        draft.category = payload;
         break;
-      case SET_START_DATE:
+      }
+      case SET_START_DATE: {
+        draft.startDate = payload;
         break;
-      case SET_END_DATE:
+      }
+      case SET_END_DATE: {
+        draft.endDate = payload;
         break;
+      }
       default:
         break;
     }
