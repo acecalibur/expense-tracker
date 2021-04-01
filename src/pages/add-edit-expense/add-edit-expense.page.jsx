a131cc8ab51e65fc90b5711187915a8c53884ecd
tdubsxc <traviedubs@gmail.com>
2021-04-01 14:50:36 -0400

    Added the expense-banner component and updated the add-edit-expense page.

--- src/pages/add-edit-expense/add-edit-expense.page.jsx
+++ src/pages/add-edit-expense/add-edit-expense.page.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ExpenseBanner from '../../components/expenses/expense-banner/expense-banner.component';
 import ExpenseForm from '../../components/expenses/expense-form/expense-form.component';
 
 const AddEditExpense = () => {
+  const { pathname } = useLocation();
+  const title = pathname.includes('add') ? 'Add Expense' : 'Edit Expense';
+
   return (
-    <div style={{ paddingTop: '2rem', paddingBottom: '4rem' }}>
-      <h2 style={{ textAlign: 'center' }}>Add Edit Expense Page</h2>
+    <div>
+      <ExpenseBanner title={title} />
       <ExpenseForm />
     </div>
   );
