cdd90ae39399f9296e166d2b5db68c3008e72314
tdubsxc <traviedubs@gmail.com>
2021-04-01 19:30:44 -0400

    Updated the not-found and dashboard pages as well as the header component.

--- src/pages/dashboard/dashboard.page.jsx
+++ src/pages/dashboard/dashboard.page.jsx
@@ -1,19 +1,20 @@
-import { Space } from 'antd';
 import React from 'react';
 import { useSelector } from 'react-redux';
-import ExpenseInputFilters from '../../components/expenses/expense-input-filters/expense-input-filters.component.jsx';
-import ExpenseList from '../../components/expenses/expense-list/expense-list.component.jsx';
-import { getVisibleExpenses } from '../../store/selectors.js';
+import ExpenseInputFilters from '../../components/expenses/expense-input-filters/expense-input-filters.component';
+import ExpenseList from '../../components/expenses/expense-list/expense-list.component';
+import ExpenseSummary from '../../components/expenses/expense-summary/expense-summary.component';
+import { selectVisibleExpenses } from '../../store/selectors';
 
 const Dashboard = () => {
-  const visibleExpenses = useSelector(getVisibleExpenses);
+  const visibleExpenses = useSelector(selectVisibleExpenses);
 
   return (
     <main>
-      <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+      <ExpenseSummary expenses={visibleExpenses} />
+      <div className="container">
         <ExpenseInputFilters />
         <ExpenseList expenses={visibleExpenses} />
-      </Space>
+      </div>
     </main>
   );
 };
--- src/pages/not-found/not-found.page.jsx
+++ src/pages/not-found/not-found.page.jsx
@@ -1,9 +1,18 @@
+import cn from 'classnames';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './not-found.module.scss';
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className={styles.box}>
+      <div className={styles.textContent}>
+        <strong>404</strong>
+        <span>The page you requested was not found.</span>
+      </div>
+      <Link to="/dashboard" className={cn('btn-info', styles.notFound)}>
+        Go to Dashboard
+      </Link>
     </div>
   );
 };
