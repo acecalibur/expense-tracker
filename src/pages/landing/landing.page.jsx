c26af656401efe33518ca5038af286c53129e8fa
tdubsxc <traviedubs@gmail.com>
2021-04-02 13:15:27 -0400

    Updated the landing page.

--- src/pages/landing/landing.page.jsx
+++ src/pages/landing/landing.page.jsx
@@ -1,9 +1,22 @@
+import { Button } from 'antd';
+import cn from 'classnames';
 import React from 'react';
+import styles from './landing.module.scss';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className={styles.box}>
+      <div className={styles.imageBox}>
+        <img src="/assets/img/landing.svg" alt="Company logo" />
+      </div>
+      <div className={styles.content}>
+        <h1>Expense Tracker</h1>
+        <span className={styles.description}>The simplest way to manage and keep track of your expenses!</span>
+        <div>
+          <Button className={cn('btn', styles.auth)}>Sign Up</Button>
+          <Button className={cn('btn', styles.auth)}>Sign In</Button>
+        </div>
+      </div>
     </div>
   );
 };
