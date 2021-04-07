fd1c326981c1d4834f9d8a0a3830ac89c74ae886
tdubsxc <traviedubs@gmail.com>
2021-04-07 09:45:57 -0400

    Updated the expense-summary component.

--- src/components/expenses/expense-summary/expense-summary.component.jsx
+++ src/components/expenses/expense-summary/expense-summary.component.jsx
@@ -1,13 +1,23 @@
 import cn from 'classnames';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { formatter } from '../../../utils/expenses.utils.js';
 import styles from './expense-summary.module.scss';
 
-const ExpenseSummary = () => {
+const expensesTotal = (expenses) => expenses.map((expense) => expense.amount).reduce((acc, val) => acc + val, 0);
+
+const ExpenseSummary = ({ expenses }) => {
+  const numOfExpenses = expenses.length;
+  const total = expensesTotal(expenses);
+  const formattedTotal = formatter.format(total);
+
   return (
     <section className={styles.box}>
       <div className="container">
-        <h2 className={styles.title}>Viewing X expenses totalling Y</h2>
+        <h2 className={styles.title}>
+          Viewing {numOfExpenses} {numOfExpenses === 1 ? 'expense' : 'expenses'} totalling{' '}
+          <strong>{formattedTotal}</strong>
+        </h2>
         <Link to="/add-expense" className={cn('btn-info', styles.addExpense)}>
           Add Expense
         </Link>
