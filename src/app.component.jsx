8705e87b445c56c24ebbc35ade8cf91680e7e95b
tdubsxc <traviedubs@gmail.com>
2021-04-07 21:45:26 -0400

    Added private and public routing components.

--- src/app.component.jsx
+++ src/app.component.jsx
@@ -1,42 +1,41 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/header/header.component';
-import Loader from './components/misc/loader/loader.component';
+import ModalManager from './components/misc/modal/modal-manager.component';
+import PrivateRoute from './components/router/private-route.component';
+import PublicRoute from './components/router/public-route.component';
 import AddEditExpense from './pages/add-edit-expense/add-edit-expense.page';
 import Dashboard from './pages/dashboard/dashboard.page';
 import Landing from './pages/landing/landing.page';
 import NotFound from './pages/not-found/not-found.page';
-import { asyncSelector } from './store/slices/async.slice';
 import { startSetExpenses } from './store/slices/expenses.slice.js';
 
 const App = () => {
   const dispatch = useDispatch();
-  const { loading, initialized } = useSelector(asyncSelector);
 
   useEffect(() => {
     dispatch(startSetExpenses());
   }, [dispatch]);
 
-  if (loading || (!loading && !initialized)) return <Loader />;
-
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={Landing} />
+    <>
+      <ModalManager />
+      <PublicRoute exact path="/" component={Landing} />
       <Route
         path={'/(.+)'}
         render={() => (
           <>
             <Header />
             <Switch>
-              <Route exact path="/dashboard" component={Dashboard} />
-              <Route exact path={['/add-expense', '/manage-expense/:id']} component={AddEditExpense} />
-              <Route path="*" component={NotFound} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+              <PrivateRoute path={['/add-expense', '/manage-expense/:id']} component={AddEditExpense} />
+              <PrivateRoute path="*" component={NotFound} />
             </Switch>
           </>
         )}
       />
-    </BrowserRouter>
+    </>
   );
 };
 
