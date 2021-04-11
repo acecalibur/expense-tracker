342d4adb80c69829d72cdf26d28f6cc8faeb28d6
tdubsxc <traviedubs@gmail.com>
2021-04-11 18:45:29 -0400

    Updated the dashboard and landing page components.

--- src/pages/dashboard/dashboard.page.jsx
+++ src/pages/dashboard/dashboard.page.jsx
@@ -3,12 +3,17 @@ import { useSelector } from 'react-redux';
 import ExpenseInputFilters from '../../components/expenses/expense-input-filters/expense-input-filters.component';
 import ExpenseList from '../../components/expenses/expense-list/expense-list.component';
 import ExpenseSummary from '../../components/expenses/expense-summary/expense-summary.component';
+import Loader from '../../components/misc/loader/loader.component';
 import { selectVisibleExpenses } from '../../store/selectors';
+import { asyncSelector } from '../../store/slices/async.slice';
 
 const Dashboard = () => {
   const visibleExpenses = useSelector(selectVisibleExpenses);
+  const { isLoading } = useSelector(asyncSelector);
 
-  return (
+  return isLoading ? (
+    <Loader />
+  ) : (
     <main>
       <ExpenseSummary expenses={visibleExpenses} />
       <div className="container">
--- src/pages/landing/landing.page.jsx
+++ src/pages/landing/landing.page.jsx
@@ -11,9 +11,10 @@ const Landing = () => {
   return (
     <div className={styles.box}>
       <div className={styles.imageBox}>
-        <img src="/assets/img/landing.svg" alt="Company logo." />
+        <img src="/assets/img/landing.svg" alt="" />
       </div>
       <div className={styles.content}>
+        <img src="/assets/img/app-logo.png" alt="Company logo." />
         <h1>Expense Tracker</h1>
         <span className={styles.description}>The simplest way to manage and keep track of your expenses!</span>
         <div>
